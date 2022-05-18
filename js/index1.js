d4549eb3bc4cb9a9d397ecd08bc6223765b5d926
Ebenezer Abegunde <66616568+iamebenezer@users.noreply.github.com>
2022-05-18 17:47:43 +0100

    Update index1.js

--- js/index1.js
+++ js/index1.js
@@ -112,7 +112,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "19 may 2022";
+const newYears = "20 may 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
